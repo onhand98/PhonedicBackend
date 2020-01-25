2ee1710b97a12cf5c762219d8a66ca3760656d7a
Chansoo Park <kevinpark@Chansoos-MacBook-Pro.local>
2020-01-25 09:41:14 +0700

    Image datamodel, uploadPost api Complete

--- src/api/Post/uploadPost/uploadPost.js
+++ src/api/Post/uploadPost/uploadPost.js
@@ -7,27 +7,48 @@ export default {
         const {
           user: { id }
         } = request;
-        const { category, title, mainImage, content } = args;
+        const { category, title, mainImage, content, images } = args;
         if (isUser(request) && (category === "POST" || category === "QNA")) {
-          return await prisma.createPost({
+          const post = await prisma.createPost({
             category,
             title,
             mainImage,
             content,
             user: { connect: { id } }
           });
+          images.forEach(
+            async image =>
+              await prisma.createImage({
+                url: image,
+                post: {
+                  connect: {
+                    id: post.id
+                  }
+                }
+              })
+          );
         } else if (
           isAuthor(request) &&
           (category === "NEWS" ||
             category === "REVIEWS" ||
             category === "PHONES")
         ) {
-          return await prisma.createPost({
+          const post = await prisma.createPost({
             category,
             title,
             mainImage,
             content,
             user: { connect: { id } }
+          });
+          images.forEach(async image => {
+            await prisma.createImage({
+              url: image,
+              post: {
+                connect: {
+                  id: post.id
+                }
+              }
+            });
           });
         } else {
           throw Error("Wrong Access");
